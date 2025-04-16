5e8945b06cd0d2d28b6ad040529bee2c99f60728
Letícia Silva <leticiati021@gmail.com>
2025-04-16 00:13:41 -0300

    3

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 
 const Header = () => {
   const router = useRouter();
